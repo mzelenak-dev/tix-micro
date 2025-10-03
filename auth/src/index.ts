7d660464171985a7f1be751cc499efa0fda38c89
zedaenious <matt@zelenak.me>
2025-10-03 19:44:49 -0400

    /api/users/currentuser dev route running

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+import type { Request, Response } from 'express'; // ✅ Type-only import
+
 const app = express();
 app.use(bodyParser.json());
 
-app.listen(3000, () => { console.log('AUTH ON 3000 SUCKA') });
+type CurrentUserResponse = {
+  id: string;
+  email: string;
+};
+
+app.get(
+  '/api/users/currentuser',
+  (req: Request, res: Response<CurrentUserResponse>) => {
+    res.send({ id: '123', email: 'user@example.com' });
+  }
+);
+
+app.listen(3000, () => {
+  console.log('AUTH ON 3000');
+});
