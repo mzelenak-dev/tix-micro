07e2172575808f9ab1a4d7bd3a9fb61d77af5f0b
zedaenious <matt@zelenak.me>
2025-10-21 14:49:36 -0400

    rmv comments from user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,24 +1,19 @@
 import mongoose from "mongoose";
 
-// mongoose wont work with ts natively, so need extra logic to force
-// type checking when creating new User documents
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-// the template model for the User
 interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
-// the instance of a User model == UserDoc
 interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
 
-// mongo specific data schema for User
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -35,12 +30,5 @@ userSchema.statics.build = (attrs: UserAttrs) => {
 }
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
-
-// const user = User.build({
-//   email: 'a@b.com',
-//   password: '1829sis8'
-// });
-
-// console.log(user, user.email, user.password);
 
 export { User };
